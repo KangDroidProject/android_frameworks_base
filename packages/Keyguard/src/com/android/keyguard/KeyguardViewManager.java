2852213099e430938d27277286dd95fe6a2c7a82
KangDroid <kangdroid@naver.com>
2014-11-20 20:36:04 +0000

    Lockscreen Notification

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -132,6 +132,10 @@ public class KeyguardViewManager {
             sendToSleep(mContext);
         };
     };
+     private NotificationHostView mNotificationView;
+     private NotificationViewManager mNotificationViewManager;
+     private boolean mLockscreenNotifications = true;
+ 
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback = new KeyguardUpdateMonitorCallback() {
         @Override
@@ -180,6 +184,8 @@ public class KeyguardViewManager {
                     Settings.System.LOCKSCREEN_SEE_THROUGH), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_BLUR_RADIUS), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS), false, this);
         }
 
         @Override
@@ -191,16 +197,26 @@ public class KeyguardViewManager {
                 hide();
             }
             mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
+          }
+      }
+
+    private void updateSettings() {
+      mLockscreenNotifications = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_NOTIFICATIONS, mLockscreenNotifications ? 1 : 0) == 1;
+        if(mLockscreenNotifications && mNotificationViewManager == null) {
+            mNotificationViewManager = new NotificationViewManager(mContext, this);
+        } else if(!mLockscreenNotifications && mNotificationViewManager != null) {
+            mNotificationViewManager.unregisterListeners();
+            mNotificationViewManager = null;
         }
+
+        mSeeThrough = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1;
+        mBlurRadius = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_BLUR_RADIUS, mBlurRadius);
+        if(!mSeeThrough) mCustomImage = null;
     }
 
-     private void updateSettings() {
-         mSeeThrough = Settings.System.getInt(mContext.getContentResolver(),
-                 Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1;
-         mBlurRadius = Settings.System.getInt(mContext.getContentResolver(),
-                 Settings.System.LOCKSCREEN_BLUR_RADIUS, mBlurRadius);
-         if(!mSeeThrough) mCustomImage = null;
-    }
 
     /**
      * @param context Used to create views.
@@ -802,10 +818,19 @@ public class KeyguardViewManager {
         mKeyguardView.initializeSwitchingUserState(options != null &&
                 options.getBoolean(IS_SWITCHING_USER));
 
+         if (mLockscreenNotifications) {
+             mNotificationView = (NotificationHostView)mKeyguardView.findViewById(R.id.notification_host_view);
+             mNotificationViewManager.setHostView(mNotificationView);
+             mNotificationViewManager.onScreenTurnedOff();
+             mNotificationView.addNotifications();
+         }
+
         // HACK
         // The keyguard view will have set up window flags in onFinishInflate before we set
         // the view mediator callback. Make sure it knows the correct IME state.
         if (mViewMediatorCallback != null) {
+             if (mLockscreenNotifications)
+                 mNotificationView.setViewMediator(mViewMediatorCallback);
             KeyguardPasswordView kpv = (KeyguardPasswordView) mKeyguardView.findViewById(
                     R.id.keyguard_password_view);
 
@@ -946,6 +971,9 @@ public class KeyguardViewManager {
         if (mKeyguardView != null) {
             mKeyguardView.onScreenTurnedOff();
         }
+         if (mLockscreenNotifications) {
+             mNotificationViewManager.onScreenTurnedOff();
+         }
         mHandler.removeCallbacks(mSmartCoverTimeout);
     }
 
@@ -995,6 +1023,9 @@ public class KeyguardViewManager {
                 Slog.w(TAG, "Exception calling onShown():", e);
             }
         }
+         if (mLockscreenNotifications) {
+             mNotificationViewManager.onScreenTurnedOn();
+         }
     }
 
     public synchronized void verifyUnlock() {
@@ -1008,6 +1039,10 @@ public class KeyguardViewManager {
      */
     public synchronized void hide() {
         if (DEBUG) Log.d(TAG, "hide()");
+
+         if (mLockscreenNotifications) {
+             mNotificationViewManager.onDismiss();
+         }
 
         if (mKeyguardHost != null) {
             mKeyguardHost.setVisibility(View.GONE);
